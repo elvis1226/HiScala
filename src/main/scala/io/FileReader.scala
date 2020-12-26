7ba6f59891f04b60699c1687cd47fc294ee4c461
Elvis.Du <jackdgf56@gmail.com>
2020-12-26 15:29:45 +0800

    first commit

---
+++ src/main/scala/io/FileReader.scala
@@ -0,0 +1,19 @@
+
+import scala.io.Source
+
+object FileReader {
+
+    def main(args: Array[String]) = {
+       val lines = Source.fromFile("src/main/resources/simple.txt").getLines().toList
+       //get max len
+       val maxLen = lines.reduceLeft(
+         (x:String,y:String) =>  if (x.length > y.length) x else y)
+
+       for (line <- lines) {
+         val numSpaces = maxLen.length - line.length
+         val padding = " " * numSpaces
+         println(padding + line.length + " | " + line)
+       }
+
+    }
+}
