f458458c9831646b0d8c670b953ef6c355e0a829
Elvis.Du <jackdgf56@gmail.com>
2021-01-03 12:32:34 +0800

    Add multiply and dividor

---
+++ src/main/scala/type/Rational.scala
@@ -0,0 +1,39 @@
+package `type`
+
+class Rational (a : Int, b : Int){
+  require(b != 0, "can't be zero")
+
+  private val g = gcd(a, b)
+  val _a = a/g
+  val _b = b/g
+
+  private def gcd(a: Int, b: Int) : Int = {
+    if (b == 0) a
+    else gcd(b, a%b)
+  }
+
+  override def toString: String = _a + "/" + _b
+  def + (r: Rational) : Rational = new Rational(_a * r._b + _b * r._a, _b * r._b)
+  def - (r: Rational) : Rational = new Rational(_a * r._b - _b * r._a, _b * r._b)
+  def * (r: Rational) : Rational = new Rational(_a * r._a , _b * r._b)
+  def / (r: Rational) : Rational = this * new Rational(r._b, r._a)
+}
+
+object Rational {
+
+  def main(argv: Array[String]) : Unit = {
+    val v1 = new Rational(4, 8)
+    val v2 = new Rational(6, 9)
+    val v3 = v1 + v2
+    println(v1 + " + " + v2 + " = " +v3)
+
+    val v4 = v1-v2
+    println(v1 + " - " + v2 + " = " +v4)
+
+    val v5 = v1*v2
+    println(v1 + " * " + v2 + " = " +v5)
+
+    val v6 = v1/v2
+    println(v1 + " / " + v2 + " = " +v6)
+  }
+}
