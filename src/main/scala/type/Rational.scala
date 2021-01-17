8eda1c5515139275f4af9537f14c917e9670815e
Elvis.Du <jackdgf56@gmail.com>
2021-01-17 20:55:33 +0800

    add overload fun for int

--- src/main/scala/type/Rational.scala
+++ src/main/scala/type/Rational.scala
@@ -14,9 +14,14 @@ class Rational (a : Int, b : Int){
 
   override def toString: String = _a + "/" + _b
   def + (r: Rational) : Rational = new Rational(_a * r._b + _b * r._a, _b * r._b)
+  def + (n: Int) : Rational = new Rational(_a + _b * n, _b)
   def - (r: Rational) : Rational = new Rational(_a * r._b - _b * r._a, _b * r._b)
+  def - (n: Int) : Rational = new Rational(_a - _b * n, _b)
   def * (r: Rational) : Rational = new Rational(_a * r._a , _b * r._b)
+  def * (n: Int) : Rational = new Rational(_a * n, _b)
   def / (r: Rational) : Rational = this * new Rational(r._b, r._a)
+  def / (n: Int) : Rational = this * new Rational(1, n)
+
 }
 
 object Rational {
@@ -35,5 +40,18 @@ object Rational {
 
     val v6 = v1/v2
     println(v1 + " / " + v2 + " = " +v6)
+
+    val v7 = v1 + 2
+    println(v1 + " + 2" + " = " +v7)
+
+    val v8 = v1 - 2
+    println(v1 + " - 2" + " = " +v8)
+
+    val v9 = v1 * 2
+    println(v1 + " * 2" + " = " +v9)
+
+    val v10 = v1 / 2
+    println(v1 + " / 2" + " = " +v10)
+
   }
 }
